dc2c8e1f14226a1b5a36b250343b1c136fcfb368
Sai Mounik <sai.mounik@gdn-commerce.com>
2025-10-15 11:18:16 +0530

    thyme leaf

--- src/main/java/com/demo/spring/springDemo/controller/UiController.java
+++ src/main/java/com/demo/spring/springDemo/controller/UiController.java
@@ -1,5 +1,7 @@
 package com.demo.spring.springDemo.controller;
 
+import com.demo.spring.springDemo.repository.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +9,17 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
 public class UiController {
+    @Autowired
+    ProductRepo productRepo;
 
     @GetMapping("/hello")
     public String viewProducts( Model model) {
         model.addAttribute("hello","Hello CSE");
         return "Hello";
+    }
+    @GetMapping("/displayProducts")
+    public String displayProducts( Model model) {
+        model.addAttribute("products",productRepo.findAll());
+        return "productlist";
     }
 }
